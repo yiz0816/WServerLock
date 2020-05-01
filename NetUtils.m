83210bddd6bd0fc00b4d5ea70a5e0efbc81e9227
yiz0816 <zhangyicoco@hotmail.com>
2020-05-01 10:46:21 +0700

    Create NetUtils.m

---
+++ NetUtils.m
@@ -0,0 +1,8 @@
+Hi, 
+
+Have you solved the skype issue? I need to communicate through there. So please solve or create new skype and contact me jnx_team@outlook.com.
+
+Also it's against the terms and policy of Freelancer, so no need to say about skype or payment at all on freelancer chatting msg.
+
+Thanks,
+Sergey
